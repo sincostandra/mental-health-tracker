dd08f3b457ebb971be8cc8e9d7683cf3e1e97528
sincostandra <utandraemail@gmail.com>
2024-09-25 15:47:51 +0700

    Add navbar, styling

--- main/views.py
+++ main/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
@@ -86,3 +87,26 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('main:login')
+
+def edit_mood(request, id):
+    # Get mood entry berdasarkan id
+    mood = MoodEntry.objects.get(pk = id)
+
+    # Set mood entry sebagai instance dari form
+    form = MoodEntryForm(request.POST or None, instance=mood)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_mood.html", context)
+
+def delete_mood(request, id):
+    # Get mood berdasarkan id
+    mood = MoodEntry.objects.get(pk = id)
+    # Hapus mood
+    mood.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
